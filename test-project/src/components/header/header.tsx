e848abf8ad75b0935890a27891e60e2e59391ce5
Julia Antoshkina <julia.antoshkina@gmail.com>
2023-09-08 18:12:23 +0200

    Added media screen to main page

--- test-project/src/components/header/header.tsx
+++ test-project/src/components/header/header.tsx
@@ -1,12 +1,14 @@
 function Header(): JSX.Element {
     return (
         <header className='header'>
-            <div className='logo-container'>
-                <img src='./img/logo.png' alt='Logo' />
-            </div>
-            <div className='buttons-container'>
-                <button className='contacts'>Контакты</button>
-                <button className='log-in'>Войти</button>
+            <div className='header-wrapper'>
+                <div className='logo-container'>
+                    <img src='./img/logo.png' alt='Logo' />
+                </div>
+                <div className='buttons-container'>
+                    <button className='contacts'>Контакты</button>
+                    <button className='log-in'>Войти</button>
+                </div>
             </div>
         </header>
     );
